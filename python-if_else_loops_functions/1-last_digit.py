a3a3f697568a9d6f6489203578bf9a46b4b817c6
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-02 17:41:08 +0000

    now the negative isn't working, adjusted

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,15 +4,13 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-# Construct the output string with a space before the condition
-output = f"Last digit of {number} is {last_digit} "
-
-if last_digit > 5:
-    output += "and is greater than 5"
-elif last_digit == 0:
-    output += "and is 0"
+# Adjust the condition based on whether the number is negative
+if number < 0:
+    condition = "less than 6 and not 0"
 else:
-    output += "and is less than 6 and not 0"
+    condition = "greater than 5" if last_digit > 5 else "0" if last_digit == 0 else "less than 6 and not 0"
+
+output = f"Last digit of {number} is {last_digit} {condition}"
 
 print(output)
 
