79224bb18e9d79c3063c93a000be889a7edc4042
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-02 16:48:29 +0000

    correcting errors in 1-last digit code

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,10 @@ print(f"last digit of {number} is {last_digit} and is greater than 5")
 if last_digit > 5:
     print(" and is greater than 5")
 elif last_digit == 0:
-    print(" and is 0")
+    output += "and is 0"
 elif last_digit < 6 and last_digit != 0:
-    print(" and is less than 6 and not 0")
+    output += "and is less than 6 and not 0"
+
+    print(output)
 
 print()
