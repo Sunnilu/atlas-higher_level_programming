485597477af748ba22ca68d9169b0024be6be50d
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-02 16:39:29 +0000

    had to add a greater than 5 statement

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 # calculate the last digit
 last_digit = abs(number) % 10
-print(f"last digit of {number} is {last_digit}")
+print(f"last digit of {number} is {last_digit} and is greater than 5")
 if last_digit > 5:
     print(" and is greater than 5")
 elif last_digit == 0:
