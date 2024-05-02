0ab3c96890a7b1d5864ec77988627920bc4f1995
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-02 18:09:40 +0000

    struggle to get neg and pos correct

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr.bin/python3
 import random
 
 number = random.randint(-10000, 10000)
@@ -7,11 +7,12 @@ if number < 0:
 else:
     last_digit = number % 10
 if last_digit > 5:
-    print("last digit of {:d} is {:d} and is greater than 5".format{number, last_digit})
-elif last_digit < 6 and last_digit != 0:
-    print("last digit of {:d} is {:d} and is less than 6 and not 0".format{number, last_digit})
+    print("last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
+elif last_digit < 6 and last_digit!= 0:
+    print("last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
 else:
-    print("last digit of {:d} is 0 and is 0".format{number})
+    print("last digit of {:d} is 0 and is 0".format(number))
+
 
 
 
