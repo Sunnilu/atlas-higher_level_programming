1ca881788225fb4fe8b9957c3fc1c85a8188fd54
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-02 17:34:10 +0000

    forgot to indent

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,16 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-print(f"Last digit of {number} is {last_digit}")
+# Construct the output string with a space before the condition
+output = f"Last digit of {number} is {last_digit} "
 
 if last_digit > 5:
-    print(" and is greater than 5")
+    output += "and is greater than 5"
 elif last_digit == 0:
-    print(" and is 0")
+    output += "and is 0"
 else:
-    print(" and is less than 6 and not 0")
+    output += "and is less than 6 and not 0"
+
+print(output)
+
 
