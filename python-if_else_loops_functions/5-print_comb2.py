e83c841cc0a38cd27d611f3233dddb9724a1dbc7
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-03 12:36:25 +0000

    more than 1 loops

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-    # Corrected approach to meet the task requirements
-for i in range(99):  # Loop from 0 to 98
-    print("{}, {}".format(i, hex(i)[2:].zfill(2)), end=', ')  # Corrected zfill to 2 for hexadecimal representation
-print("99")  # Print the number 99 in decimal
-
+for i in range(99): 
+    print("{}, {}".format(i, hex(i)[2:].zfill(2)), end=', ')  
+print("99")
