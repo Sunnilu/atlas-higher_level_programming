3ac876a97311ae188ef94060817f6e3b72902bd1
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-03 13:19:03 +0000

    changed format again

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-# Generate all possible sums of pairs of numbers from 1 to 9
-sums = [i + j for i in range(1, 10) for j in range(i+1, 10)]
-
-# Sort the sums in ascending order
-sums.sort()
-
-# Print the sorted sums in the specified format
-for sum_ij in sums:
-    print("{:02d}".format(sum_ij), end=", " if sum_ij!= 89 else "\n")
-
-
+for x in range(0, 10);
+    for y in range(x + 1, 10);
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
 
