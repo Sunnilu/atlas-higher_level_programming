014e4e3198e4a6e77af367d6fbff24806b70b46d
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-03 12:24:24 +0000

    still printing long format corrected

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{}, {}".format(i, hex(i)[2:].zfill(2)), end=', ')
-    print("99")
+    # Corrected approach to meet the task requirements
+for i in range(99):  # Loop from 0 to 98
+    print("{}, {}".format(i, hex(i)[2:].zfill(2)), end=', ')  # Corrected zfill to 2 for hexadecimal representation
+print("99")  # Print the number 99 in decimal
+
