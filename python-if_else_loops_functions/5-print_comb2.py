b4a88fb913e76de6ecb8a536110be8556821737e
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-03 12:53:28 +0000

    rewrite of code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Using a single loop to iterate from 0 to 98 and print both decimal and hexadecimal representations
-for i in range(99):  # Loop from 0 to 98
-    print("{}, {}".format(i, hex(i)[2:].zfill(2)), end=', ')  # Print decimal and hexadecimal representation
-print("99")  # Print the number 99 in decimal
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
+
 
