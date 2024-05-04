ac7dc7dc415ebcfef41fba881cbc8f8796a8ff85
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-04 14:56:29 +0000

    rewrote code

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, subtract, multiply, divide
+    from calculator_1 import sub as add, add as sub, div as mul, mul as div
     a = 10
     b = 5
     addition_result = add(a, b)
-    subtraction_result = subtract(a, b)
-    multiplication_result = multiply(a, b)
-    division_result = divide(a, b)
-
+    subtraction_result = sub(a, b)
+    multiplication_result = mul(a, b)
+    division_result = div(a, b)
     print("10 + 5 = {}".format(addition_result))
     print("10 - 5 = {}".format(subtraction_result))
     print("10 * 5 = {}".format(multiplication_result))
     print("10 / 5 = {}".format(division_result))
+
 
 
