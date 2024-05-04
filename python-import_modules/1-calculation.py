a6a87fa6efad733311244b37bccf9c75995dcf7e
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-04 14:37:58 +0000

    corrected indentation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if _name_ == "_main_":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-     addition_result = add(a, b)
+    addition_result = add(a, b)
     subtraction_result = subtract(a, b)
     multiplication_result = multiply(a, b)
     division_result = divide(a, b)
