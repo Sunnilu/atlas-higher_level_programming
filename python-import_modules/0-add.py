00cf9bea5ff9a01ececbeb446ebafe04d692616d
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-04 14:19:15 +0000

    unable to meet checks rewrote

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-if '_name_' == '_main_':
+if __name__ == "__main__":
     from add_0 import add
+
     a = 1
     b = 2
-    results = add(a, b)
-    print("{} + {} = {}".format(a, b, results))
+    result = add(a, b)
+    print('{} + {} = {}'.format(a, b, result))
+
