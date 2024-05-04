47b7fbc7b1102a1aa0f20d14d7a03912126e5170
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-04 14:36:38 +0000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+     addition_result = add(a, b)
+    subtraction_result = subtract(a, b)
+    multiplication_result = multiply(a, b)
+    division_result = divide(a, b)
+
+    print("10 + 5 =", addition_result)
+    print("10 - 5 =", subtraction_result)
+    print("10 * 5 =", multiplication_result)
+    print("10 / 5 =", division_result)
