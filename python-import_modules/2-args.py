406e998d676b3351071f79b9f4da42b644c89139
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 18:22:36 +0000

    udate to code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    x = len(argv) - 1
-    if x < 1:
-        print("{} arguments.".format(x))
+    num_args = len(argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+    for i, arg in enumerate(argv[1:], 1):
+        print("{}: {}".format(i, arg))
 
