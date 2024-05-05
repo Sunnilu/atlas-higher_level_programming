2c1913a7ecec436efd2925b4b5bb0c99b3ac2439
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 19:27:58 +0000

    had to paste due to whitespaces

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from  hidden_4 import *
+    from hidden_4 import *
     allf = dir()
     for i in range(0, len(allf)):
         if allf[i][:2] != "__":
-            jls_extract_var = "{:s}"
-            def jls_extract_def():
-                return jls_extract_var
+            print("{}".format(allf[i]))
 
-
-            def jls_extract_def():
-                return jls_extract_def
-
-
-            print(jls_extract_def()().format(allf[i]))
 
