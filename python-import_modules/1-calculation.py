aa8c415a116ad57026b19fe0581bc84b8416ad5f
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 10:35:44 +0000

    rewrite

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 # Import specific functions from calculator_1.py
 from calculator_1 import add, subtract, multiply, divide
 
 # Define variables a and b
 a = 10
 b = 5
-
-# Call and print the results of the imported functions
-result_add = add(a, b)
-result_subtract = subtract(a, b)
-result_multiply = multiply(a, b)
-result_divide = divide(a, b)
-
 # Print the results, adhering to the constraint of using print no more than 4 times
-print(f"Addition: {result_add}")
-print(f"Subtraction: {result_subtract}")
-print(f"Multiplication: {result_multiply}")
-print(f"Division: {result_divide}")
+print("Addition: {}".format(result_add))
+print("Subtraction: {}".format(result_subtract))
+print("Multiplication: {}".format(result_multiply))
+print("Division: {}".format(result_divide))
+
+
 
 
 
