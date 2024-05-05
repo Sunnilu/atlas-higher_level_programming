ab0f0f55df51932e603c67a749c49e2b8520b390
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 22:26:43 +0000

    updates spelling

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i,num in enumeate(row):
+        for i,num in enumerate(row):
             if i != 0:
                 print(" ", end="")
             
