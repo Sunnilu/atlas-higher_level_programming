9a689e5d30e6f526e86b67a9ff98ab7a56c75671
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 19:24:19 +0000

    install def jls_extract

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,14 @@ if __name__ == "__main__":
     allf = dir()
     for i in range(0, len(allf)):
         if allf[i][:2] != "__":
-            print("{:s}".format(allf[i]))
+            jls_extract_var = "{:s}"
+            def jls_extract_def():
+                return jls_extract_var
+
+
+            def jls_extract_def():
+                return jls_extract_def
+
+
+            print(jls_extract_def()().format(allf[i]))
 
