26ae3909e28ec3f727060e06bcfa8cc48a326ed7
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 10:38:56 +0000

    still working on code with div

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,12 @@ from calculator_1 import add, subtract, multiply, divide
 
 # Define variables a and b
 a = 10
-b = 5
+b = 
+# Call and store the results of the imported functions
+result_add = add(a, b)
+result_subtract = subtract(a, b)
+result_multiply = multiply(a, b)
+result_divide = divide(a, b)
 # Print the results, adhering to the constraint of using print no more than 4 times
 print("Addition: {}".format(result_add))
 print("Subtraction: {}".format(result_subtract))
