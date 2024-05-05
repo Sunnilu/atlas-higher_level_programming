1e177375f1a92f5a10924f2fd598fd196cdf7f7a
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-05 22:24:41 +0000

    update to enumerate

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
+        for i,num in enumeate(row):
+            if i != 0:
+                print(" ", end="")
+            
             print("{:d}".format(num), end=" ")
         print()
