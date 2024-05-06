e9740bd84b1173887775f3e34f531dfe3a758e57
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 03:37:01 +0000

    rewrite

--- python-data_structures/11-main.py
+++ python-data_structures/11-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    return new_list
