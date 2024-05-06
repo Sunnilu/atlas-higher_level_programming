0c227678353788ddb15ead2c82ed0ff0c09644c7
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 23:32:00 +0000

    syntax errors

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for item in my_list:
+    for i in range(len(my_list)):  # Define 'i' within the loop
         try:
-            if isinstance(item, int):
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
                 if count == x:
                     break
-        except TypeError:
+        except IndexError:
+            # This block is intentionally left empty because we're not supposed to handle non-integer values explicitly
             pass
     return count
+
 
 
 
