98b8276e9d07ccf40d3a5d58b36722dafeffef07
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 11:44:03 +0000

    added e to new_list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
         if element == search:
             new_list.append(replace)
         else:
-            nw_list.append(element)
+            new_list.append(element)
     return new_list
