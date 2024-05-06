50ce0b79515ca6a102dbd24d9c81cbb16e60d441
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 23:36:57 +0000

    removed len

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for _ in my_list:
-        count += 1
+    # First, count the number of integers in the list
+    for item in my_list:
+        if isinstance(item, int):
+            count += 1
+    # Now, print the integers up to x
     for item in my_list:
         try:
             if isinstance(item, int):
@@ -13,6 +16,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             pass
     return count
+
 
 
 
