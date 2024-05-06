5bb7d473defa9387e7a3b0bd81aa6a70ed91288a
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 12:48:45 +0000

    udated code

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
-        sorted_keys = sorted(a_dictionary.keys())
-        for key in sorted_keys:
-            print("{}: {}".format(key, a_dictionary[key]))
+    a_dictionary[key] = value
+
+# Example usage:
+my_dict = {'a': 1, 'b': 2}
+update_dictionary(my_dict, 'c', 3)  # Adds a new key-value pair
+update_dictionary(my_dict, 'a', 10) # Updates the value of an existing key
+print(my_dict)  # Output: {'a': 10, 'b': 2, 'c': 3}
+
+
