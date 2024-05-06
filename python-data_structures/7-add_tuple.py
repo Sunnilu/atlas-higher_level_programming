b3b729a6085a709009bea86dc7b6537327e99747
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 02:54:58 +0000

    forgot to add [1] to line 6

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
     tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
     
-    return (tuple_a[0] + tuple_b[0], tuple_a + tuple_b[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
