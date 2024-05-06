177f127d6ff23e6d00e0bc7be6234f6391eeb923
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 12:40:08 +0000

    updated 7-udate

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if a_dictionary is not None:
+        for key in sorted_keys:
+            print("{}: {}".format(key, a_dictionary[key]))
