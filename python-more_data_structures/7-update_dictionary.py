c22c94f99f3d5f1c557a881e51128b5da8959153
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 13:01:21 +0000

    7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-
-# Example usage:
-my_dict = {'a': 1, 'b': 2}
-update_dictionary(my_dict, 'c', 3)  # Adds a new key-value pair
-update_dictionary(my_dict, 'a', 10) # Updates the value of an existing key
-print(my_dict)  # Output: {'a': 10, 'b': 2, 'c': 3}
-
-
+    a-dictionary[key] = value
