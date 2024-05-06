bda212a20bd1e956c88e3cd564f95c7ac68ab528
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 12:43:10 +0000

    updated sorted_keys

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary is not None:
+ sorted_keys = sorted(a_dictionary.keys())
         for key in sorted_keys:
             print("{}: {}".format(key, a_dictionary[key]))
