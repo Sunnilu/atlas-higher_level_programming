04da44e073595b82111b7c17af49304534fb26e2
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 22:39:25 +0000

    changed value to v

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+        if isinstance(value, (list, set, tuple)):
+            for v in value:
+                try:
+                    print("{:d}".format(v))
+                    return True
+                except ValueError:
+                    continue
+        else:
+            print("{d:}".format(value))
+            return True
+        except TypeError:
+            return False
