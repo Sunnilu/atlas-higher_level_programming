fcd29182e50fe72b61e8b47170039be2fce5527f
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 02:32:05 +0000

    rewrote len(row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i,num in enumerate(row):
-            if i != 0:
+            if i != len(row) - 1:
                 print(" ", end="")
             
             print("{:d}".format(num), end=" ")
