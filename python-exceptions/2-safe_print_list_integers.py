1c8a664ebbd9ad2a69c9b6c83702e9eab6c49ce5
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 23:34:33 +0000

    this code is problamatic

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(len(my_list)):  # Define 'i' within the loop
+    for _ in my_list:
+        count += 1
+    for item in my_list:
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-                if count == x:
+            if isinstance(item, int):
+                print("{:d}".format(item), end="")
+                count -= 1
+                if count == 0:
                     break
-        except IndexError:
-            # This block is intentionally left empty because we're not supposed to handle non-integer values explicitly
+        except TypeError:
             pass
     return count
+
 
 
 
