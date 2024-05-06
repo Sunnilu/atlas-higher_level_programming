2c88177a188f094cd1429a585b811cb1893edb3e
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 13:06:50 +0000

    7-updat_dicionary rewrite

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a-dictionary[key] = value
+    # Check if the key exists in the dictionary
+    if key in a_dictionary:
+        # If the key exists, update its value
+        a_dictionary[key] = value
+    else:
+        # If the key doesn't exist, add the new key/value pair
+        a_dictionary[key] = value
+
+    return a_dictionary
+
+
