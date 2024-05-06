06cd1c85a7f7524eb71b1e1e0cf9fae30d838838
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 23:27:32 +0000

    added end=

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for item in my_list:
         try:
             if isinstance(item, int):
-                print("{:d}".format(item))
+                print("{:d}".format(my_list[i]), end="")
                 count += 1
                 if count == x:
                     break
