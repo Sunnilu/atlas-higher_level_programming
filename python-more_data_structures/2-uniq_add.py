8c8255efbdb2bf99ab18cb7cba4789e449f5c5b3
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-06 11:50:45 +0000

    2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    sum_unique = 0
+    for num in my_list:
+        if num no in unique_integers:
+            unique_integers.add(num)
+            sum_unique += num
+    return sum_unique
