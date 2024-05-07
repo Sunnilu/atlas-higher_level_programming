2a0baa7fd468a5cb5c9d727d612302ecfddfa906
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-07 13:35:45 +0000

    indentationError corrected

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,11 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for item in my_list:
-        if isinstance(item, int):
-            print("{:d}".format(item), end=" ")
-            count += 1
-            if count == x:
-            break
+            if isinstance(item, int):
+                print("{:d}".format(item), end=" ")
+                count += 1
+                if count == x:
+                    break
         print()
     except:
         raise ValueError("x is bigger than the length of my_list")
