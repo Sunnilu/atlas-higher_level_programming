64ec901a1d5d89d6aab4ab13ed807b9ab233f927
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-07 00:12:22 +0000

    output not correct printing len

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,9 +10,11 @@ def safe_print_list_integers(my_list=[], x=0):
                     break
     except TypeError:
         pass
-    if count < x:
-        raise ValueError("out of range")
+    finally:
+        if count < x:
+            raise ValueError("out of range")
     return count
+
 
 
 
