0761743782f66a7f69dde12c5cf2f5f019f65e46
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-10 16:34:39 +0000

    chmod 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    ''' defines a square specific size
+    attributes: _size (int): the size of the square's size '''
+    def__init__(self, size):
+    ''' initialize a new square object
+    parameters:
+    _size (int): the size of the square's side. '''
+    self._size = size
