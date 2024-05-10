95531b0118845908a1b41f39db7ba4348b4805a8
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-10 16:18:29 +0000

    changed s to S in Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' this module defines a square shape '''
-class square:
+class Square:
     ''' represents a specified square length.
      Attributes: side_length (int): the length of the square's sides '''
 
