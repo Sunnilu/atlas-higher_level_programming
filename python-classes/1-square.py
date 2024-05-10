9d4794c7c601b7d2b2b993a2bed48c968976851c
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-10 16:38:44 +0000

    indention errors corrected

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,7 @@ class Square:
     ''' defines a square specific size
     attributes: _size (int): the size of the square's size '''
     def __init__(self, size):
-    ''' initialize a new square object
-    parameters:
-    _size (int): the size of the square's side. '''
-    self._size = size
+       ''' initialize a new square object
+       parameters:
+       _size (int): the size of the square's side. '''
+       self._size = size
