ab25aa8c3d66e134ee6e7b7685e0f76579d7bf54
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-10 16:09:31 +0000

    chmod 0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 ''' this module defines a square shape '''
 class square:
-    ''' represents a specified square length. Attributes: side_length (int): the length of the square's sides '''
+    ''' represents a specified square length.
+     Attributes: side_length (int): the length of the square's sides '''
 
     pass
 
