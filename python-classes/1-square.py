c7be6e48f98c630fe6ab469a850a2e67a50dd6ae
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-10 16:36:36 +0000

    did not indent between def _

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,7 +2,7 @@
 class Square:
     ''' defines a square specific size
     attributes: _size (int): the size of the square's size '''
-    def__init__(self, size):
+    def __init__(self, size):
     ''' initialize a new square object
     parameters:
     _size (int): the size of the square's side. '''
