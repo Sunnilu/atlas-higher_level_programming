73b1b6f8f70abd570e1832f366bd99f9b91345e5
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-11 13:20:45 +0000

    added to code

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,4 +7,15 @@ class Square:
        parameters:
        _size (int): the size of the square's side. '''
        self.__size = size
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, new_size):
+        if new_size > 0:
+            self.__size = new_size
+        else:
+            print("Error: size must be a positive integer.")
+    def area(self):
+        return self.__size ** 2
+
     
