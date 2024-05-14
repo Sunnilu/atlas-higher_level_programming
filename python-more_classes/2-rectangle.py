1c2ac4018bbe5cc03d5a268775ae2d94591b6e35
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-14 13:57:36 +0000

    errors corrected

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -18,9 +18,8 @@ class Rectangle:
         height(value): property setter to set the height of the rectangle.
     '''
 
-    class Rectangle:
-        def __init__(self, width=0, height=0):
-            """Initializes a new Rectangle instance with optional width and height."""
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle instance with optional width and height."""
         if not isinstance(width, int) or width < 0:
             raise ValueError("width must be an integer >= 0")
         if not isinstance(height, int) or height < 0:
@@ -86,3 +85,4 @@ class Rectangle:
             return 0
         return 2 * (self._width + self._height)
 
+    
