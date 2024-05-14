059ad92b2cf10495a39ddd61416e0e6dc5183ae6
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-14 14:54:51 +0000

    rewrite code would not work

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -14,59 +14,125 @@ class Rectangle:
     Methods:
         width: property to get the width of the rectangle.
         width(value): property setter to set the width of the rectangle.
-        height: property to get the height of the rectangle.
+        height: property to get the height of the rectangl.
         height(value): property setter to set the height of the rectangle.
     '''
 
+
     def __init__(self, width=0, height=0):
-        self._width = width
-        self._height = height
+        """
+        Initialize a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle (default is 0).
+            height (int): The height of the rectangle (default is 0).
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self._width
+        """
+        Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The new width value.
+
+        Raises:
+            TypeError: If the width is not an integer.
+            ValueError: If the width is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        """
+        Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The new height value.
+
+        Raises:
+            TypeError: If the height is not an integer.
+            ValueError: If the height is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
 
     def area(self):
-        return self._width * self._height
+        """
+        Calculate the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.width * self.height
 
     def perimeter(self):
-        if self._width == 0 or self._height == 0:
+        """
+        Calculate the perimeter of the rectangle.
+
+        Returns:
+            int: The perimeter of the rectangle.
+        """
+        if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self._width + self._height)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        if self._width == 0 or self._height == 0:
+        """
+        Return a string representation of the rectangle.
+
+        Returns:
+            str: A string representing the rectangle using '#' characters.
+        """
+        if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join(["#" * self._width] * self._height)
+        return '\n'.join(['#' * self.width] * self.height)
 
     def __repr__(self):
-        return f"Rectangle({self._width}, {self._height})"
+        """
+        Return a string representation of the rectangle for recreation.
+
+        Returns:
+            str: A string representation of the rectangle.
+        """
+        return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
+        """Print a message when the rectangle instance is deleted."""
         print("Bye rectangle...")
 
-my_rectangle = Rectangle(2, 4)
-print(f"Area: {my_rectangle.area()} - Perimeter: {my_rectangle.perimeter()}")
+   
+
+
 
 
 
