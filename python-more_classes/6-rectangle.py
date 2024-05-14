b96fcf57a408522e625856bda4614622a4cec715
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-14 23:35:12 +0000

    added @width.setter

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -22,8 +22,13 @@ class Rectangle:
 
     
     @property
+    def width(self):
+        '''getter for the private instance attribute width'''
+        returnself.__width
+
+    @width.setter
     def width(self, value):
-        '''setter for the private instance attribute width'''
+        '''private instance attribute width'''    
         if type(value) is not int:
            raise TypeError("width must be an integer")
         if value < 0:
