51169b8cc7395b66d3ed6c92e520cd6521c6c1da
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-15 00:03:53 +0000

    corrected 6 not 1 this time

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -2,42 +2,30 @@
 '''3-rectangle: class Rectangle
 '''
 
-
 class Rectangle:
-    
     '''Represents a rectangle with specified width and height'''
-    
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        '''initializes the rectangle'''
+        '''Initializes the rectangle'''
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
-    @height.setter
-    def height(self, value):
-        '''setter for the private instance attribute of height'''
-        if type(value) is not int:
-           raise TypeError("height must be an integer")
-        if value < 0:
-           raise ValueError("height must be >= 0")
-    self.__height = value
-
     def __del__(self):
-        '''prints a string when an instance has been deleted'''
+        '''Prints a string when an instance has been deleted'''
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    
     @property
     def width(self):
-        '''getter for the private instance attribute width'''
+        '''Getter for the private instance attribute width'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''setter for the private instance attribute of width'''
+        '''Setter for the private instance attribute width'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -46,6 +34,16 @@ class Rectangle:
 
     @property
     def height(self):
-        '''getter for the private instance attribute height'''
+        '''Getter for the private instance attribute height'''
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Setter for the private instance attribute height'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
 
