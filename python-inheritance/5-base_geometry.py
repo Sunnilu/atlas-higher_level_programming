ee802718042b40a33fa8756d565e520cf46fd4b8
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-19 23:28:44 +0000

    added args

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -2,5 +2,18 @@
 ''' a module that defines the base geometry'''
 
 class BaseGeometry:
+    """
+    A base class for geometric operations.
+
+    This class provides a foundation for defining geometric operations
+    and properties. It serves as a base for more specific geometric classes.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
     pass
+
 
