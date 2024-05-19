36492b84731ee606df3853195345c01f6ed4c143
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-19 23:08:10 +0000

    indentation errors

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,19 +3,21 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """
-    check if obj is an instance of a_class or a subclass thereof.
-
-   Args:
-   obj: any python object
-   a_class: a python class
-
-   Returns: 
-   true is the object is an instance of a_class or it's subclass; otherwise false
-   """
-
-   return isinstance(obj, a_class)
    
+
+   """
+    Returns True if the object is an instance of, or if the object is an instance of a class that inherited from,
+    the specified class; otherwise False.
+
+    Args:
+    obj: Any Python object.
+    a_class: A Python class.
+
+    Returns:
+    True if obj is an instance of a_class or its subclass; otherwise False.
+    """
+    return isinstance(obj, a_class)
+
 
     
 
