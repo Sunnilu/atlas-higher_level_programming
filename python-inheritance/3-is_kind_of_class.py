48c01d25a9f2d70a030694bd8606c38aa0177c33
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-19 22:45:01 +0000

    3-is

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''a module that defines a kind of class'''
+
+
+def is_kind_of_class(obj, a class):
+    '''
+    check if obj is an instance of a_class or a subclass thereof.
+
+    Parameters:
+    obj (object): the object to check.
+    a_class (type): the class to compare against.
+
+    Returns:
+    bool: true if obj is an instance of a_class or a subclass thereof, false otherwise
+    '''
+
+    # get the class of the object
+    obj_class = type(obj)
+
+    #start travesing u the inheritance chain
+    current_class = obj_class
+    while current_class is not None:
+        #check if the current cass is the target class
+        if current_class is a_class:
+            return True
+
+        # move up the inheritance chain
+        current_class = current_class.bases__[0]
+
+    # if we haven't returned yet, the object isn't an instance therefore
+    return False
