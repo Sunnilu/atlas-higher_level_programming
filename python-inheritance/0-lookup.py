5666a77053e085270509d27f969b2238409834c4
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-19 01:28:15 +0000

    added comment after python3

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""modue for lookup method"""
 def lookup(obj):
     """Returns the list of available attributes and methods of an object.
 
@@ -7,7 +8,7 @@ def lookup(obj):
 
     returns:
     a list of strings containing the names of attributes and methods of the object"""
-    
+
     return dir(obj)
 
 
