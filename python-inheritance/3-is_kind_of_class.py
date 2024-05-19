aa6696b532ee6e7e408953448cbdc9e3ce48c42b
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-19 23:09:40 +0000

    more indent

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -16,7 +16,7 @@ def is_kind_of_class(obj, a_class):
     Returns:
     True if obj is an instance of a_class or its subclass; otherwise False.
     """
-    return isinstance(obj, a_class)
+return isinstance(obj, a_class)
 
 
     
