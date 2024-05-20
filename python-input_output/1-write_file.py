46e573ca42b7974bb14c1967f6b2a713feca05a4
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-20 19:56:10 +0000

    to many blank lines

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,7 +2,6 @@
 '''Reads a text file.'''
 
 
-
 def write_file(filename="", text=""):
     '''Writes a string to a text file (UTF8) and returns the number of characters written.'''
     with open(filename, mode='w', encoding='utf-8') as file:
