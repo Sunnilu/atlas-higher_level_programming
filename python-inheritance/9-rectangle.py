aaa8ca85cd52e66186b409bf0d97f92389f34e46
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-20 16:53:45 +0000

    9-rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Method for a Rectangle class.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''a subclass representing a rectangle.'''
+    def __init__(self, width, height):
+        '''constructor'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''method which returns area of rectangle.'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''string representation method.'''
+        return "[rectangle] " + str(self._-width) + "/" + str(self._-height)
+
+
