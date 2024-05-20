b72bee92025c1a19f695384a31ecc7acd71efc7e
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-20 17:34:56 +0000

    added height

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -5,11 +5,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     '''A sublass representing a rectangle.'''
-def __init__(self, size):
+def __init__(self, size, height):
     '''Constructor'''
     self.integer_validator("size", size)
+    self.integer_validator("height", height)
     self.__size = size
-    super().__init__(size, size)
+    self.__height = height
+    super().__init__(size, size, height)
 
 def area(self):
     '''Method for area of square.'''
