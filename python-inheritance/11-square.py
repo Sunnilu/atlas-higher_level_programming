7b12c61e6a80fd2618f41e4583471039c6cdf1f6
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-20 17:40:28 +0000

    removed height

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,21 +2,24 @@
 '''Module for Rectangle class.'''
 Rectangle = __import__('9-rectangle').Rectangle
 
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
-    '''A sublass representing a rectangle.'''
-def __init__(self, size, height):
-    '''Constructor'''
-    self.integer_validator("size", size)
-    self.integer_validator("height", height)
-    self.__size = size
-    self.__height = height
-    super().__init__(size, height)
+    '''A subclass representing a square.'''
 
-def area(self):
-    '''Method for area of square.'''
-    return self.__size ** 2
+    def __init__(self, size):
+        '''Constructor'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
 
-def __str__(self):
-    '''returns string representation of this square.'''
-    return "[square] " + str(self.__size) + "/" + str(self.__size)
+    def area(self):
+        '''Method for area of square.'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''Returns string representation of this square.'''
+        return "[Square] {}/{}".format(self.__size, self.__size)
