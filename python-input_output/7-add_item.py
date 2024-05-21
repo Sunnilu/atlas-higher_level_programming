28aed1e7c3fa11156f7918b715846af6fc4ef30a
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-21 22:09:36 +0000

    attempt to remove Holberton

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -17,14 +17,12 @@ def main():
         data = load_from_json_file(file_name)
 
     # Add command-line arguments to the list (excluding "Holberton")
-    for arg in sys.argv[1:]:
-        if arg != "Holberton":
-            data.append(arg)
+    args = [arg for arg in sys.argv[1:] if arg != "Holberton"]
+    data.extend(args)
 
     # Save the list to a JSON file
     save_to_json_file(data, file_name)
-    print("Items added to add_item.json:", [arg for arg in sys.argv[1:] if arg != "Holberton"])
+    print("Items added to add_item.json:", args)
 
 if __name__ == "__main__":
     main()
-
