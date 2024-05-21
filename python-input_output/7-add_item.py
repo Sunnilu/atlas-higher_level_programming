39685284f1a9bb50f03f983880649a7a51d2c4a1
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-21 22:30:40 +0000

    added documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,14 +7,24 @@ from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 class MyClass:
+    """A simple class representing an example object."""
+
     def __init__(self, name, age):
+        """Initialize the MyClass instance.
+
+        Args:
+            name (str): The name of the instance.
+            age (int): The age of the instance.
+        """
         self.name = name
         self.age = age
     
     def __dict__(self):
+        """Return a dictionary representation of the instance."""
         return {'name': self.name, 'age': self.age}
 
 def main():
+    """Main function to add items to a JSON file."""
     # Check if the file exists
     file_name = 'add_item.json'
     data = []
@@ -37,5 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
