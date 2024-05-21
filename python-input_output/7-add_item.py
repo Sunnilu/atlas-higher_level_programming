47dbabc8d9b984dcc236e9f7cfecc07a7b5b7f56
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-21 22:29:09 +0000

    added an instance

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,22 +2,40 @@
 ''' module to add'''
 
 import sys
-from 5_save_to_json_file import save_to_json_file
-from 6_load_from_json_file import load_from_json_file
+from os import path
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
+class MyClass:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __dict__(self):
+        return {'name': self.name, 'age': self.age}
 
 def main():
-    # Initialize an empty list to store the arguments
-    args_list = []
-    
-    # Iterate over the command-line arguments starting from index 1 (0 is the script name)
-    for arg in sys.argv[1:]:
-        # Add each argument to the list
-        args_list.append(arg)
-    
-    # Use the save_to_json_file function to save the list to a file
-    save_to_json_file(args_list, "add_item.json")
+    # Check if the file exists
+    file_name = 'add_item.json'
+    data = []
+
+    # If the file exists, load its content
+    if path.exists(file_name):
+        data = load_from_json_file(file_name)
+
+    # Add command-line arguments to the list (excluding "Holberton")
+    args = [arg for arg in sys.argv[1:] if arg != "Holberton"]
+    data.extend(args)
+
+    # Add an instance of MyClass to the list
+    my_instance = MyClass("John", 30)
+    data.append(my_instance.__dict__())
+
+    # Save the list to a JSON file
+    save_to_json_file(data, file_name)
+    print("Items added to add_item.json:", args)
 
 if __name__ == "__main__":
     main()
+
 
