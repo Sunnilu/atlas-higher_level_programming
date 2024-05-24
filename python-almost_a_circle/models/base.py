6505d408960824df122010dcf6602c55cd6331d3
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-24 17:09:08 +0000

    rewrite of base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,95 +1,39 @@
+test_base.py
 #!/usr/bin/python3
 
+
+import unittest
 
 from models.base import Base
 
 
-class Rectangle(Base):
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        super().__init__(id)
+class TestBase(unittest.TestCase):
 
-    @property
-    def width(self):
-        return self.__width
+    def test_initialization(self):
 
-    @width.setter
-    def width(self, value):
-        self.setter_validation("width", value)
-        self.__width = value
+        base1 = Base()
 
-    @property
-    def height(self):
-        return self.__height
+        base2 = Base()
 
-    @height.setter
-    def height(self, value):
-        self.setter_validation("height", value)
-        self.__height = value
+        self.assertEqual(base1.id, 1)
 
-    @property
-    def x(self):
-        return self.__x
+        self.assertEqual(base2.id, 2)
 
-    @x.setter
-    def x(self, value):
-        self.setter_validation("x", value)
-        self.__x = value
 
-    @property
-    def y(self):
-        return self.__y
+    def test_saving_id(self):
 
-    @y.setter
-    def y(self, value):
-        self.setter_validation("y", value)
-        self.__y = value
+        base = Base(100)
 
-    def area(self):
-        return (self.height * self.width)
+        self.assertEqual(base.id, 100)
 
-    def display(self):
-        rectangle = ""
-        print("\n" * self.y, end="")
-        for i in range(self.height):
-            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
-        print(rectangle, end="")
 
-    def update(self, *args, **kwargs):
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
-        try:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
-            self.y = args[4]
-        except IndexError:
-            pass
+    def test_to_json_string_valid(self):
 
-    def to_dictionary(self):
-        return {'x': getattr(self, "x"),
-                'y': getattr(self, "y"),
-                'id': getattr(self, "id"),
-                'height': getattr(self, "height"),
-                'width': getattr(self, "width")}
+        pass
 
-    @staticmethod
-    def setter_validation(attribute, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(attribute))
-        if attribute == "x" or attribute == "y":
-            if value < 0:
-                raise ValueError("{} must be >= 0".format(attribute))
-        elif value <= 0:
-            raise ValueError("{} must be > 0".format(attribute))
 
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                       self.width, self.height)
+if __name__ == '__main__':
+
+    unittest.main()
+
