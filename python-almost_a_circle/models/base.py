f943db08d4cc2b7c4f15cb0b5769e439c5ce2522
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-24 19:01:27 +0000

    models/bases

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,39 +1,14 @@
-test_base.py
 #!/usr/bin/python3
+'''method for base class'''
 
 
-import unittest
+class Base:
+    '''a representation of base OOP hierachy'''
+    __nb_objects = 0
 
-from models.base import Base
-
-
-
-class TestBase(unittest.TestCase):
-
-    def test_initialization(self):
-
-        base1 = Base()
-
-        base2 = Base()
-
-        self.assertEqual(base1.id, 1)
-
-        self.assertEqual(base2.id, 2)
-
-
-    def test_saving_id(self):
-
-        base = Base(100)
-
-        self.assertEqual(base.id, 100)
-
-
-    def test_to_json_string_valid(self):
-
-        pass
-
-
-if __name__ == '__main__':
-
-    unittest.main()
+    def __init__(self, id=None):
+        self.id = id
+    else:
+        __nb_objects += 1
+        self.id = Base.__nb_objects
 
