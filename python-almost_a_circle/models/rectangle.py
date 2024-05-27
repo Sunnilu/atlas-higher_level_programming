ef17ce7ea98a87dd627eee5f525340466dcd96d1
Suntha <Suntha.Lucas@atlasschool.com>
2024-05-27 16:05:50 +0000

    models for rectangle rewrite

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,91 +1,91 @@
 #!/usr/bin/python3
-'''Module for rectangle class'''
+'''module for rectangle class'''
 from models.base import Base
 
-
-class Rectangle:
-    '''
-    Class representing a rectangle.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-    '''
-
-    def __init__(self, width, height):
-        """
-        Initialize a new Rectangle instance.
-
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-        """
-        self._width = width
-        self._height = height
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
-        """
-        Getter for the width attribute.
-        """
         return self._width
 
     @width.setter
     def width(self, value):
-        """
-        Setter for the width attribute.
-        
-        Args:
-            value (int): New width value.
-        """
-        if isinstance(value, int) and value > 0:
-            self._width = value
-        else:
-            raise ValueError("Width must be a positive integer.")
+        self.setter_validation("width", value)
+        self._width = value
 
     @property
     def height(self):
-        """
-        Getter for the height attribute.
-        """
         return self._height
 
     @height.setter
     def height(self, value):
-        """
-        Setter for the height attribute.
-        
-        Args:
-            value (int): New height value.
-        """
-        if isinstance(value, int) and value > 0:
-            self._height = value
-        else:
-            raise ValueError("Height must be a positive integer.")
+        self.setter_validation("height", value)
+        self._height = value
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self.setter_validation("x", value)
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self.setter_validation("y", value)
+        self._y = value
 
     def area(self):
-        """
-        Calculate and return the area of the rectangle.
-        
-        Returns:
-            int: Area of the rectangle.
-        """
         return self._width * self._height
 
     def display(self):
-        """
-        Display the rectangle using asterisks (*) in the console.
-        """
-        for _ in range(self._height):
-            print("*" * self._width)
+        print("\n" * self._y, end="")
+        for i in range(self._height):
+            print(" " * self._x + "#" * self._width)
 
-# Example usage
-r = Rectangle(12, 15)
-print(f"Width: {r.width}, Height: {r.height}")
-print(f"Area: {r.area()}")
+    def update(self, *args, **kwargs):
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                setattr(self, key, val)
+            return
+        try:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
+        except IndexError:
+            pass
 
-try:
-    r.width = 20  # Changing width
-    r.display()
-except Exception as e:
-    print(e)
+    def to_dictionary(self):
+        return {
+            'x': getattr(self, "x"),
+            'y': getattr(self, "y"),
+            'id': getattr(self, "id"),
+            'height': getattr(self, "height"),
+            'width': getattr(self, "width")
+        }
+
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value)!= int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
