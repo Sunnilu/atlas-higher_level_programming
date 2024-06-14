af8f31da5c54343df100f7fdbb5c15531decdfed
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-14 01:28:38 +0000

    added comments

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+#prompt for the MYSQL root password
 read -s -p "Enter MySQL root password:" PASSWORD
+#execute the SHOW DATABASES command and print the output
 mysql -h localhost -u root -p"$password" --silent -e "SHOW DATABASES;"
