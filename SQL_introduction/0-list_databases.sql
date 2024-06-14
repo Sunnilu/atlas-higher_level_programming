7808363b0cc8267ccddbf6c9516afa331faea9f6
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-14 13:59:34 -0500

    Update 0-list_databases.sql
    REwrite of 0-list databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,2 @@
-#!/bin/bash
-#prompt for the MYSQL root password
-read -s -p "Enter MySQL root password:" PASSWORD
-#execute the SHOW DATABASES command and print the output
-mysql -h localhost -u root -p"$password" --silent -e "SHOW DATABASES;"
+# Listing all databases for MySQL_server
+SHOW DATABASES;
