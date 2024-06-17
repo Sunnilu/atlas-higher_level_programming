b02db36ac8a09435a523741d0000aaa72339c8bf
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-17 09:46:29 -0500

    Update 9-full_creation.sql
    Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,8 @@
--- create table and insert multiple registers
--- Execute: cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- create table 'second_table' in database 'hbtn_0c_0
+-- Add props (id INT), (name VARCHAR(256), (score INT) 
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
+
+-- Script should create a maximum of 4 records
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
