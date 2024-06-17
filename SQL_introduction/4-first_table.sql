9d6100589e0e4c328fa879d0a1bb0f0e3da16a10
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-17 09:27:28 -0500

    Update 4-first_table.sql
    create id and name VARCHAR 256

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- Create a first_table in database
--- Execute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- Create a table called first_table
+--'first_table' should have (id INT) and (name VARCHAR(256))
 CREATE TABLE IF NOT EXITS first_table (id INT, NAME VARCHAR(256);
