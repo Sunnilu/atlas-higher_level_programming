f167ee5ee31554898ebdd0ec7b584f9df6838ca2
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-17 09:36:43 -0500

    Update 7-insert_value.sql
     inserts a new row in the table first_table

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- Insert values
--- Execute: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- Insert a new record in a row in 'first_table' from database hbtn_0c_0
 INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
