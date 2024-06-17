57961a71a250b4d453ac1b8cfd4a565621a5c440
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-17 10:28:32 -0500

    Update 4-first_table.sql
    missing '

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- Create a table called first_table
+-- Create a table called 'first_table'
 --'first_table' should have (id INT) and (name VARCHAR(256))
 CREATE TABLE IF NOT EXITS first_table (id INT, name VARCHAR(256));
