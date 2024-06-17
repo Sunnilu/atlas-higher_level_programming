e776d757badfe84330c37c5bbf31314a09cb13cb
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-17 09:49:36 -0500

    Update 10-top_score.sql
    script that lists all records of the table second_table of the database hbtn_0c_

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,2 @@
--- list by best score
--- Execute: cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- List all_records in 'second_table' of database 'hbtn_0c_0'
 SELECT score, name FROM first_table ORDER BY score DESC;
