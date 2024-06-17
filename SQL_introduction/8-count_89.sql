ece434124dafa158fae0ad1481f2f28199558042
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-17 09:40:16 -0500

    Update 8-count_89.sql
    script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
--- count rows with id 89
---Execute: cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
+-- Of database 'hbtn_0c_0' in my MySQL_server
 SELECT COUNT (*) FROM first_table WHERE id=89;
