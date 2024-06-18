58b6dd909f956c79ae0c5d888a7762d8f8fd6189
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 10:19:00 -0500

    Update 11-genre_id_all_shows.sql
    rewrite

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,6 @@
 -- list all shows contained in database hbtn_0d_tvshows
 
-SELECT tv_shows.title, genres.genre_name 
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN genres ON tv_show_genres.genre_id = genres.id
-ORDER BY tv_shows.title, genres.genre_name;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
