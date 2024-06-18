ead7fa02b8b462653948036c90f03ba742a9adab
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 10:34:19 -0500

    Update 13-count_shows_by_genre.sql
    added DESC

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- list all genres contained in hbtn_0d & number of shows linked 2 each
 
-CREATE VIEW name AS
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
