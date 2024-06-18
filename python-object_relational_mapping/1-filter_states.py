b356459e1ee0182e2beca5eab459f9a562877220
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-18 18:04:05 +0000

    1-filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,10 +2,49 @@
 """Lists states"""
 
 import MySQLdb
-from sys import argv
+import sys
+
+def list_states_starting_with_n(username, password, database):
+    # Connect to MySQL server
+    try:
+        conn = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+        cursor = conn.cursor()
+
+        # Execute SQL query to retrieve states starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        
+        # Fetch all rows
+        rows = cursor.fetchall()
+        
+        # Print results as specified
+        for row in rows:
+            print(row)
+
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="host=localhost", port=3306, user=argv[1],
-                            passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute
+    # Check for correct number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    # Get MySQL credentials from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call function to list states starting with 'N'
+    list_states_starting_with_n(username, password, database)
+
