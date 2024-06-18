30ac53727f158ff8b703a5e515a242e966a7e94e
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-18 00:59:54 +0000

    making files

---
+++ SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,3 @@
+102-rating_shows.sql 101-not_a_comedy.sql 100-not_my_genres.sql 16-shows_by_genre.sql 15-comedy_only.sql 14-my_genres.sql 13-count_shows_by_genre.sql 12-no_genre.sql 11-genre_id_all_shows.sql 10-genre_id_by_show.sql 9-cities_by_state_join.sql 8-cities_of_california_subquery.sql 7-cities.sql 6-states.sql 5-unique_id.sql 4-never_empty.sql 3-force_name.sql 2-create_read_user.sql 1-create_user.sql 0-privileges.sql
+
+
