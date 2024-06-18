2d1da051ff9136714aee58eb7e575631e2b1e45c
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 10:32:31 -0500

    Update 13-count_shows_by_genre.sql
    correct sytax

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- list all genres contained in hbtn_0d & number of shows linked 2 each
 
-CREATE name AS genre, COUNT(*) AS number_of_shows fROM tv_genres
-JOIN tv-show_genres ON id=tv_showgenres.genre_id
-GROUP BY tv_show_genres.genre_id
+CREATE VIEW name AS
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
