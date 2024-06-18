707d5dfdd8fc685a64ec525ab06354568bab051c
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 07:37:57 -0500

    Update 0-privileges.sql
    Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
-
-
--- List all priveleges of MySQL	
+-- List all priveleges of MySQL	pakati pa useiz user_0d_na user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
