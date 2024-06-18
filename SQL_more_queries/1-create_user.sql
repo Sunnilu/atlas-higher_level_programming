4c99d9735c0d3588e484f1b86a2beea5ca2b6ecc
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 07:49:18 -0500

    Update 1-create_user.sql
    added identifed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- Create of the MySQL server user_0d_1
-CREATE USER IF NOT EXISTS 'user-0d_1'@'localhost';
+-- Create of the MySQL server user 'user_0d_1'
+
+CREATE USER IF NOT EXISTS 'user-0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
