ba763fd9edfaa0a88fa4ea87180484a3b7c4b446
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 08:59:51 -0500

    Update 7-cities.sql
    rewrite of order

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,13 @@
--- Create database hbtn_0d_usa & table cities on my MySQL server
+-- Create the database hbtn_0d_usa if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+-- Switch to the newly created database
+USE hbtn_0d_usa;
+
+-- Create the table cities within the hbtn_0d_usa database
+CREATE TABLE IF NOT EXISTS cities (
   id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
   state_id INT NOT NULL,
-  FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-  name VARCHAR(256) NOT NULL);
+  name VARCHAR(256) NOT NULL,
+  FOREIGN KEY(state_id) REFERENCES states(id)
+);
