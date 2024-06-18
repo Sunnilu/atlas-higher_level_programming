4fae86fb7cbdb7f7714b325b9c7b969af3d56553
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 08:28:30 -0500

    Update 2-create_read_user.sql
    added to pass checker

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,13 @@
 -- Create the database of hbtn_0d_2 and user user_0d_2
 
-CREATE DATABASE IF NOT EXITS hbtn_0d_2;
-CREATE USER IF NOT EXITS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to ensure the changes take effect immediately
 FLUSH PRIVILEGES;
