7001d45b17abe51da4a06ebcc574607e5c061a3f
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 08:23:53 -0500

    Create 2-create_read_user.sql
    script that creates the database hbtn_0d_2 and the user user_0d_2.

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Create the database of hbtn_0d_2 and user_0d_2
+
+CREATE DATABASE IF NOT EXITS hbtn_0d_2;
+CREATE USER IF NOT EXITS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
