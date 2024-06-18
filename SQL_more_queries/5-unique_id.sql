761094a1266ab8d7ba78ec13f9e60fb6e1261f90
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-06-18 08:40:13 -0500

    Update 5-unique_id.sql
    added , instead of ; behind id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Creates table unique_id on my MySQL server
 
 CREATE TABLE IF NOT EXISTS unique_id
-  (id INT DEFAULT 1;
-  UNIQUE (ID);
+  (id INT DEFAULT 1,
+  UNIQUE (ID),
   name VARCHAR(256));
