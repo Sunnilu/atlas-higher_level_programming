9cd6d657e4cb33d7788a72ca494430cf13274bba
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 17:21:50 +0000

    correction to models city py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,25 +1,17 @@
-#!/usr/bin/python3
-"""Class definition of a city"""
+# model_city.py
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base  # Import the Base class from model_state module
+from sqlalchemy.orm import relationship
+from model_state import Base
 
 class City(Base):
-    """City class that inherits from Base"""
-
-    __tablename__ = 'cities'  # Table name in the database
-
-    # Columns definition
+    """City class to map to the cities table in the database."""
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-    def __repr__(self):
-        """String representation of the City object"""
-        return "<City(id={}, name='{}', state_id={})>".format(self.id, self.name, self.state_id)
+    # Establishing relationship to State
+    state = relationship("State", back_populates="cities")
 
-if __name__ == "__main__":
-    # Testing the City class
-    # You can perform operations here to test the City class behavior
-    pass
 
