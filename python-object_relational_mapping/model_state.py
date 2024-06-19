df4f79a72378f3839d0e3a92c91dd9da0ebd1b1b
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 12:48:20 +0000

    added comments

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Start link class to table in database"""
+"""Script to link a class to a table in a MySQL database."""
 
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sys import argv  # Import argv to retrieve command-line arguments
+from model_state import Base, State  # Import SQLAlchemy Base and State class
+from sqlalchemy import create_engine  # Import create_engine from SQLAlchemy
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-            argv[3]), pool_pre_ping=True)
+    # Construct the database connection string using command-line arguments
+    # argv[1]: MySQL username
+    # argv[2]: MySQL password
+    # argv[3]: Database name
+    db_connection_str = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(db_connection_str, pool_pre_ping=True)
+
+    # Create the tables defined in the Base's metadata in the database
     Base.metadata.create_all(engine)
+
