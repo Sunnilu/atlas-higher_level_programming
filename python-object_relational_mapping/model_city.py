f3307a0cacee01b8e2d7d39cb4dbe6e39b2b0f68
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 17:39:48 +0000

    model

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,19 +2,15 @@
 ''' model to fetch city by state
 '''
 
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base
+from model_base import Base
 
-class City(Base):
-    """City class to map to the cities table in the database."""
-    __tablename__ = 'cities'
+class State(Base):
+    """State class to map to the states table in the database."""
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-    # Establishing relationship to State
-    state = relationship("State", back_populates="cities")
-
-
+    # Relationship to City
+    cities = relationship("City", order_by=City.id, back_populates="state")
