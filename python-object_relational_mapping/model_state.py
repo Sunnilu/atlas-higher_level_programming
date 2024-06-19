bbf38bf6bf142c92bb7abdc53ad5cfb472fcc4f3
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 12:55:30 +0000

    added more comment

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,16 +5,16 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
-# Define City class representing another table in the database
 class City(Base):
+    """Class representing the 'cities' table in the database."""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))  # Foreign key referencing states.id
     state = relationship("State", back_populates="cities")  # Relationship with State class
 
-# Define State class with a relationship to City
 class State(Base):
+    """Class representing the 'states' table in the database."""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
