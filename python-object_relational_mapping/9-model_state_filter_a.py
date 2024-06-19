42d4d1b0fb98d78400c552f7af1cfa1ec550fcb2
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 15:48:34 +0000

    final attempt to print a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,29 +9,13 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py mysql_username mysql_password database_name")
-        sys.exit(1)
-
-    # Get MySQL credentials from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Create connection string
-    connection_string = f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}"
-
-    # Create engine and session
-    engine = create_engine(connection_string)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
+        
 
-    # Query the database for states containing letter 'a' and sort by id
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
 
-    # Print the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    session.close()
