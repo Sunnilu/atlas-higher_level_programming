ec98792ae2d953b233e287bbd6eb30563828e463
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 14:14:15 +0000

    rewrite 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,24 +2,32 @@
 '''
 Script that lists all State objects from the database - using module SQLAlchemy
 '''
-from model_state import Base, State
-from sys import argv
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State  # Adjust import as per your module structure
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
     # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2],
-    argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
     # create a Session
     session = Session()
+
+    # Ensure tables are created
     Base.metadata.create_all(engine)
 
-    s_tate = session.query(state).order_by(State.id).all()
-    for state in s_tate:
+    # Query all State objects and print them
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
         print("{}: {}".format(state.id, state.name))
+
+    # Close the session
     session.close()
