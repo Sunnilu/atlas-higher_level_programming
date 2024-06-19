04a728df9ce576213ea289e96ecdae5a5c7fc49c
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 17:06:11 +0000

    addressed print new state name

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,18 +4,38 @@ changes the name of a State object from the database hbtn_0e_6_usa
 '''
 import sys
 from sqlalchemy import create_engine
-from model_state import Base, State
-
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State  # Adjust this import according to your project structure
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    if len(sys.argv) != 4:
+        print("Usage: script.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+
+    # Database connection
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
-    session.commit()
+
+    # Create a session
+    with Session() as session:
+        try:
+            # Query the State object by id (assuming id=2 as per your example)
+            state_to_update = session.query(State).filter_by(id=2).first()
+
+            if state_to_update:
+                # Change the name attribute
+                state_to_update.name = 'New Mexico'
+
+                # Commit the change to the database
+                session.commit()
+                print("State name updated successfully.")
+            else:
+                print("State with id=2 not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            session.rollback()
+
 
 
 
