24b2365ae82f78f305980545f24be01d7bc83617
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-19 18:03:13 +0000

    rewrite for indent errors

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 '''Script to manage database operations for cities and states.'''
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.engine.url import URL
-    from model_state import Base, State
-    from model_city import City
-    from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.url import URL
+from model_state import Base, State
+from model_city import City
+from sys import argv
 
-    db = {'drivername': 'mysql+mysqldb',
-           'host': 'localhost',
-           'port': '3306',
-           'username': argv[1],
-           'password': argv[2],
-           'database': argv[3]}
+if __name__ == "__main__":
+    db = {
+        'drivername': 'mysql+mysqldb',
+        'host': 'localhost',
+        'port': '3306',
+        'username': argv[1],
+        'password': argv[2],
+        'database': argv[3]
+    }
 
     url = URL(**db)
-    engin = create_engine(url, pool_pre_ping=True)
+    engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    for state, city n session.query(State, City)\
-                            .filter(State.id == City.state_id).all():
-        print("{}: ({} {}".format(state.name, city.id, city.name))
-    session.close()
+    with Session(engine) as session:
+        try:
+            for state, city in session.query(State, City).filter(State.id == City.state_id).all():
+                print(f"{state.name}: ({city.id}) {city.name}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            session.rollback()
+        finally:
+            session.close()
