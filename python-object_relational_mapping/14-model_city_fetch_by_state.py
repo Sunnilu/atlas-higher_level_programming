e6f1c132baa57f05c3e391ec2f3b85e13f8ea741
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-20 00:04:22 +0000

    add docstring

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
 """
-prints all City objects from the database
+Script that prints all City objects from the database.
 """
 
-if __name__ == "__main__":
-    from sqlalchemy.engine import create_engine
-    from sqkakcgent.engine.url import URL 
-    from sqlalchemy.orm import Session
-    from model_state import Base, State
-    from model_city import City
-    from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+from sys import argv
 
-    db = {'drivername': 'mysql+mysqldb',
-            'host': 'localhost',
-            'port': '3306',
-            'username': argv[1],
-            'password': argv[2],
-            'database': argv[3]}
+if __name__ == "__main__":
+    # Database connection parameters
+    db = {
+        'drivername': 'mysql+mysqldb',
+        'host': 'localhost',
+        'port': '3306',
+        'username': argv[1],
+        'password': argv[2],
+        'database': argv[3]
+    }
     
+    # Constructing the URL for the database connection
     url = URL(**db)
-    engin = creat_engine(url, pool_pre_ping=True)
+    engine = create_engine(url, pool_pre_ping=True)
+    
+    # Create all tables in the database if they do not exist
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                                .filter(State.id == City.state_id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-        session.close()
-
+    # Creating a session to interact with the database
+    with Session(engine) as session:
+        try:
+            # Query all State and City objects and print them
+            for state, city in session.query(State, City).filter(State.id == City.state_id).all():
+                print(f"{state.name}: ({city.id}) {city.name}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            session.rollback()  # Rollback in case of error to avoid inconsistent state
