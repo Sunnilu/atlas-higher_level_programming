ed53732d4ba0a44a79d9bdc5e287ab939b5f4651
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-21 21:45:08 +0000

    prints the first argument passed to it

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints the first argument passed to it.
+
+const args = process.arg.slice(2);
+// checking if there are any arguments passed
+if (args[0] !== undefined) {
+    console.log(args[0]);
+} else {
+    console.log("No argument");
+}
