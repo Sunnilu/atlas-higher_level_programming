d0e6ef1d673be066961950379fc22dea8da27567
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 02:00:10 +0000

    script that computes and prints factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// define a recursive function to compute factorial.
+let args = parseInt(process.argv[2]);
+if (isNaN(args)) {
+    args = 1;
+}
+function factorial(n) {
+    if (n == 1) {
+        return 1;
+    }
+    // recursive case: n * factorial
+    return n * factorial(n - 1);
+}
+    console.log(factorial(args));
