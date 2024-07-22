ff71571b0bc33fc212a8c287303d2c54317c9d20
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 01:24:39 +0000

    defines the add function

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,3 +3,13 @@
 function add(a, b) {
     return a + b;
 }
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.agrv[3]);
+
+if (!Number.isNaN(arg1) && !Number.isNaN(arg2)) {
+    const result = add(arg1, arg2);
+    console.log(result);
+} else {
+    // print NaN if either argument is not a valid number
+    console.log("NaN");
+}
