706b1c52980ded696a44a92ed66fb7ccc226a465
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 01:39:37 +0000

    trying to define 3 still rewrite

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,16 +3,14 @@
 function add(a, b) {
     return a + b;
 }
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.agrv[3]);
+const first = process.argv[2];
+const second = process.argv[3];
 
-if (process.argv.length < 4) {
-    console.log("Missing arguments");
-} else if(!Number.isNaN(arg1) && !Number.isNaN(arg2)) {
-    // call the add function and print results
-    const result = add(arg1, arg2);
-    console.log(result);
-} else {
-    // print NaN if either argument is not a valid number
-    console.log("NaN");
+function add (a, b) {
+    return a + b;
 }
+const a = parseInt(first);
+const b = parseInt(second);
+
+console.log(add(a, b));
+
