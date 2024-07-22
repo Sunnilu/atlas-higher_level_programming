05ec0982fdcc00b02b7f2b056d00a74a98615b38
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 17:10:05 +0000

    funtion that prints the number of args already pri

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//  function that prints the number of arguments already printed and the new argument value.
+exports.logMe = function (item) {
+   if (typeof this.count === 'undefined') {
+    this.count = 0;
+   }
+   console.log(this.count + ': ' + item);
+   this.cont++;
+};
