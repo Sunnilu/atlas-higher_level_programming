569dfc167cb9126571997d65f093645cdf82de48
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 16:37:19 +0000

    square that defies a square and inherits from squa

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// a class Square that defines a square and inherits from Square.
+const supSquare = require('./5-square');
+
+class Square extends supSquare {
+    charPrint (c) {
+        if (c == null) {
+            c = 'X';
+        }
+        for (let i = 0; i < this.width; i++) {
+            console.log(c.repeat(this.width));
+        }
+    }
+}
+module.exports = Square;
