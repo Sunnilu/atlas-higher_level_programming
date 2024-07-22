a3ff887829f7a02001a4405e77198e914592983c
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 01:30:53 +0000

    '3' not defined added arg.

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -6,7 +6,10 @@ function add(a, b) {
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.agrv[3]);
 
-if (!Number.isNaN(arg1) && !Number.isNaN(arg2)) {
+if (process.argv.length < 4) {
+    console.log("Missing arguments");
+} else if(!Number.isNaN(arg1) && !Number.isNaN(arg2)) {
+    // call the add function and print results
     const result = add(arg1, arg2);
     console.log(result);
 } else {
