559ec0077759931ee9fca64710be9da0082b68a5
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 17:33:29 +0000

    reverse

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
-// a function that returns the reversed version of a list.
-exports.reverse = function (list) {
-    // Ensure list is treated as an array and lengths are compared as strings
-    const arrList = Array.isArray(list) ? list : [];
-    let start = 0;
-    let end = arrList.length - 1;
+// 8-esrever.js
 
-    while (start < end) {
-        // Swap elements
-        const temp = arrList[start];
-        arrList[start] = arrList[end];
-        arrList[end] = temp;
-
-        // Move towards the center
-        start++;
-        end--;
+exports.esrever = function(list) {
+    let reversedList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+        reversedList.push(list[i]);
     }
-
-    return arrList;
+    return reversedList;
 };
+
  
