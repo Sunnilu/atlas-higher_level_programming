b7198c2a5fc1f00a8aaa9aa06845fa0f26b5f894
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 03:15:30 +0000

    added not a number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 // Prints My Number if the first argument is successfully coverted to integer.
-const args = process.argv.slice(2);
+const args = process.argv.[2]
 // trying to convert the first argument to an integer
-const num = Number(args[0]);
-// see if the conversion is successful
-if (!isNaN(num)) {
-    console.log ('My number: ${89}');
+if (arg === undefined || isNaN(parseInt(args))) {
+    console.log ('Not a number');
 } else {
-    console.log('Not a number');
+    console.log(`My number:${parseInt(args)}`);
 }
