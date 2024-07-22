f407fc2dbf9091abc9e7ca55f3ab8dd49f4d189c
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 00:14:59 +0000

    trying to get c is cool

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // script that prints two arguments passed to it.
 
-const [, , arg1 = 'undefined', arg2 = 'undefined'] = process.argv;
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
 // checking if at least two arguments are passed
-console.log('${arg1} is ${arg2}');
+console.log(arg1 + " " + arg2); // Corrected line
