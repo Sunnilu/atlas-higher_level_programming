eb103bfcd0b69a1ce542ee195a170b91621f12da
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 00:49:49 +0000

    removed messages added lines

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 //prints three specific lines using an array of strings and a loop.
-const messages = [
+const lines = [
     "C is fun",
     "Python is cool",
     "JavaScript is amazing",
 ];
 // variable to accumulate the final output
 let output = '';
-
+// loop through the array and print each line using console.log
 for (let i = 0; i < lines.length; i++) {
     consol.e.log(lines[i]);
 }
