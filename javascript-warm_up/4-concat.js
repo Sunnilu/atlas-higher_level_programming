6ecd836476b765de802a2231860822c6f5b7c7f3
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 00:03:42 +0000

    reconstr. const to get c is cool

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 // script that prints two arguments passed to it.
 
-const args = process.argv.slice(2);
-
+const [, , arg1 = 'undefined', arg2 = 'undefined'] = process.argv;
 // checking if at least two arguments are passed
-const arg1 = args[0] || 'undefined';
-const arg2 = args[1] || 'undefined';
-
 console.log('${arg1} is ${arg2}');
