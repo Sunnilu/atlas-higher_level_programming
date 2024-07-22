07b93acbc5d370b78fb8c51e28dd2c3f66e330ec
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-22 03:17:49 +0000

    removed . after argv[2]

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-// Prints My Number if the first argument is successfully coverted to integer.
-const args = process.argv.[2]
+// Prints My Number if the first argument is successfully converted to integer.
+const args = process.argv[2]; // corrected the syntax for accessing command line arguments
+
 // trying to convert the first argument to an integer
-if (arg === undefined || isNaN(parseInt(args))) {
-    console.log ('Not a number');
+if (args === undefined || isNaN(parseInt(args))) {
+    console.log('Not a number');
 } else {
-    console.log(`My number:${parseInt(args)}`);
+    console.log(`My number: ${parseInt(args)}`); // added a space after the colon for better formatting
 }
+
