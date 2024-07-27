111d5b58ad781530d8049b773c16d8ffd52ca0fa
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-27 09:38:57 -0500

    chmod 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
+
+request.get(url, function (error, res, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        try {
+            const data = JSON.parse(body);
+            console.log(data.title);
+        } catch (e) {
+            console.log('Error parsing JSON:', e);
+        }
+    }
+});
