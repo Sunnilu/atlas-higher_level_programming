c960c3d510b756aaa63268d3e0962d194e02f955
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-27 10:35:07 -0500

    semistandard fix added

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,14 +5,14 @@ const id = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
 
 request.get(url, function (error, res, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        try {
-            const data = JSON.parse(body);
-            console.log(data.title);
-        } catch (e) {
-            console.log('Error parsing JSON:', e);
-        }
+  if (error) {
+    console.log(error);
+  } else {
+    try {
+      const data = JSON.parse(body);
+      console.log(data.title);
+    } catch (e) {
+      console.log('Error parsing JSON:', e);
     }
+  }
 });
--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,27 +6,26 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.error('Request failed:', error);
-        return;
-    }
+  if (error) {
+    console.error('Request failed:', error);
+    return;
+  }
 
-    try {
-        // Parse the body of the response
-        const data = JSON.parse(body);
-        const results = data.results;
+  try {
+    // Parse the body of the response
+    const data = JSON.parse(body);
+    const results = data.results;
 
-        // Count the number of movies featuring character ID 18
-        const count = results.reduce((acc, movie) => {
-            return movie.characters.some(character => character.endsWith('/18/'))
-                ? acc + 1
-                : acc;
-        }, 0);
+    // Count the number of movies featuring character ID 18
+    const count = results.reduce((acc, movie) => {
+      return movie.characters.some(character => character.endsWith('/18/'))
+        ? acc + 1
+        : acc;
+    }, 0);
 
-        // Output the result
-        console.log(count);
-
-    } catch (e) {
-        console.error('Failed to parse response:', e);
-    }
+    // Output the result
+    console.log(count);
+  } catch (e) {
+    console.error('Failed to parse response:', e);
+  }
 });
